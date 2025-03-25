d8c31c7c7f5c08216c71ba3e7a9a622ab9738c57
Damien Maresma <damien.maresma@mckesson.ca>
2025-03-25 15:57:50 -0400

    bring back simple-ddl-parser

--- datacontract/imports/sql_importer.py
+++ datacontract/imports/sql_importer.py
@@ -1,8 +1,7 @@
 import logging
 import os
 
-import sqlglot
-from sqlglot.dialects.dialect import Dialects
+from simple_ddl_parser import parse_from_file
 
 from datacontract.imports.importer import Importer
 from datacontract.model.data_contract_specification import DataContractSpecification, Field, Model, Server
@@ -20,12 +19,11 @@ class SqlImporter(Importer):
 def import_sql(
     data_contract_specification: DataContractSpecification, format: str, source: str, import_args: dict = None
 ) -> DataContractSpecification:
-    sql = read_file(source)
 
     dialect = to_dialect(import_args)
 
     try:
-        parsed = sqlglot.parse_one(sql=sql, read=dialect)
+        ddl = parse_from_file(source, group_by_type=True, encoding = "cp1252", output_mode = dialect )
     except Exception as e:
         logging.error(f"Error parsing SQL: {str(e)}")
         raise DataContractException(
@@ -36,104 +34,72 @@ def import_sql(
             result=ResultEnum.error,
         )
 
-    server_type: str | None = to_server_type(source, dialect)
+    server_type: str | None = dialect
     if server_type is not None:
         data_contract_specification.servers[server_type] = Server(type=server_type)
 
-    tables = parsed.find_all(sqlglot.expressions.Table)
+    tables = ddl["tables"]
 
     for table in tables:
         if data_contract_specification.models is None:
             data_contract_specification.models = {}
 
-        table_name = table.this.name
+        table_name = table["table_name"]
 
         fields = {}
-        for column in parsed.find_all(sqlglot.exp.ColumnDef):
-            if column.parent.this.name != table_name:
-                continue
-
+        for column in table["columns"]:
             field = Field()
-            col_name = column.this.name
-            col_type = to_col_type(column, dialect)
-            field.type = map_type_from_sql(col_type)
-            col_description = get_description(column)
-            field.description = col_description
-            field.maxLength = get_max_length(column)
-            precision, scale = get_precision_scale(column)
-            field.precision = precision
-            field.scale = scale
-            field.primaryKey = get_primary_key(column)
-            field.required = column.find(sqlglot.exp.NotNullColumnConstraint) is not None or None
-            physical_type_key = to_physical_type_key(dialect)
-            field.config = {
-                physical_type_key: col_type,
-            }
+            field.type = map_type_from_sql(map_type_from_sql(column["type"]))
+            if not column["nullable"]:
+                field.required = True
+            if column["unique"]:
+                field.unique = True
+            
+            if column["size"] is not None and column["size"] and not isinstance(column["size"], tuple):
+                field.maxLength = column["size"]
+            elif isinstance(column["size"], tuple):
+                field.precision = column["size"][0]
+                field.scale = column["size"][1]
 
-            fields[col_name] = field
+            field.description = column["comment"][1:-1] if column.get("comment") else None
+            field.required = column["nullable"]
+            if column.get("with_tag"):
+                field.tags = ", ".join(column["with_tag"])
+            if column.get("with_masking_policy"):
+                field.classification = ", ".join(column["with_masking_policy"]) 
+            if column.get("generated"):
+                field.examples = str(column["generated"])
+            field.unique = column["unique"]
 
+            fields[column["name"]] = field
+            
+            if table.get("constraints"):
+                if table["constraints"].get("primary_key"):                   
+                    for primary_key in table["constraints"]["primary_key"]["columns"]:
+                        if primary_key in fields:
+                            fields[primary_key].unique = True
+                            fields[primary_key].required = True
+                            fields[primary_key].primaryKey = True
+
+            table_description = table["comment"][1:-1] if  table.get("comment") else None
+            table_tags = table["with_tag"][1:-1] if  table.get("with_tag") else None
+            
         data_contract_specification.models[table_name] = Model(
             type="table",
+            description=table_description,
+            tags=table_tags,
             fields=fields,
         )
 
     return data_contract_specification
 
-
-def get_primary_key(column) -> bool | None:
-    if column.find(sqlglot.exp.PrimaryKeyColumnConstraint) is not None:
-        return True
-    if column.find(sqlglot.exp.PrimaryKey) is not None:
-        return True
-    return None
-
-
-def to_dialect(import_args: dict) -> Dialects | None:
+def to_dialect(import_args: dict) -> str | None:
     if import_args is None:
         return None
     if "dialect" not in import_args:
         return None
     dialect = import_args.get("dialect")
-    if dialect is None:
-        return None
-    if dialect == "sqlserver":
-        return Dialects.TSQL
-    if dialect.upper() in Dialects.__members__:
-        return Dialects[dialect.upper()]
-    if dialect == "sqlserver":
-        return Dialects.TSQL
-    return None
-
-
-def to_physical_type_key(dialect: Dialects | None) -> str:
-    dialect_map = {
-        Dialects.TSQL: "sqlserverType",
-        Dialects.POSTGRES: "postgresType",
-        Dialects.BIGQUERY: "bigqueryType",
-        Dialects.SNOWFLAKE: "snowflakeType",
-        Dialects.REDSHIFT: "redshiftType",
-        Dialects.ORACLE: "oracleType",
-        Dialects.MYSQL: "mysqlType",
-        Dialects.DATABRICKS: "databricksType",
-    }
-    return dialect_map.get(dialect, "physicalType")
-
-
-def to_server_type(source, dialect: Dialects | None) -> str | None:
-    if dialect is None:
-        return None
-    dialect_map = {
-        Dialects.TSQL: "sqlserver",
-        Dialects.POSTGRES: "postgres",
-        Dialects.BIGQUERY: "bigquery",
-        Dialects.SNOWFLAKE: "snowflake",
-        Dialects.REDSHIFT: "redshift",
-        Dialects.ORACLE: "oracle",
-        Dialects.MYSQL: "mysql",
-        Dialects.DATABRICKS: "databricks",
-    }
-    return dialect_map.get(dialect, None)
-
+    return dialect
 
 def to_col_type(column, dialect):
     col_type_kind = column.args["kind"]
@@ -142,61 +108,11 @@ def to_col_type(column, dialect):
 
     return col_type_kind.sql(dialect)
 
-
 def to_col_type_normalized(column):
     col_type = column.args["kind"].this.name
     if col_type is None:
         return None
     return col_type.lower()
-
-
-def get_description(column: sqlglot.expressions.ColumnDef) -> str | None:
-    if column.comments is None:
-        return None
-    return " ".join(comment.strip() for comment in column.comments)
-
-
-def get_max_length(column: sqlglot.expressions.ColumnDef) -> int | None:
-    col_type = to_col_type_normalized(column)
-    if col_type is None:
-        return None
-    if col_type not in ["varchar", "char", "nvarchar", "nchar"]:
-        return None
-    col_params = list(column.args["kind"].find_all(sqlglot.expressions.DataTypeParam))
-    max_length_str = None
-    if len(col_params) == 0:
-        return None
-    if len(col_params) == 1:
-        max_length_str = col_params[0].name
-    if len(col_params) == 2:
-        max_length_str = col_params[1].name
-    if max_length_str is not None:
-        return int(max_length_str) if max_length_str.isdigit() else None
-
-
-def get_precision_scale(column):
-    col_type = to_col_type_normalized(column)
-    if col_type is None:
-        return None, None
-    if col_type not in ["decimal", "numeric", "float", "number"]:
-        return None, None
-    col_params = list(column.args["kind"].find_all(sqlglot.expressions.DataTypeParam))
-    if len(col_params) == 0:
-        return None, None
-    if len(col_params) == 1:
-        if not col_params[0].name.isdigit():
-            return None, None
-        precision = int(col_params[0].name)
-        scale = 0
-        return precision, scale
-    if len(col_params) == 2:
-        if not col_params[0].name.isdigit() or not col_params[1].name.isdigit():
-            return None, None
-        precision = int(col_params[0].name)
-        scale = int(col_params[1].name)
-        return precision, scale
-    return None, None
-
 
 def map_type_from_sql(sql_type: str):
     if sql_type is None:
@@ -218,14 +134,16 @@ def map_type_from_sql(sql_type: str):
         return "string"
     elif sql_type_normed.startswith("ntext"):
         return "string"
+    elif sql_type_normed.startswith("number"):
+        return "decimal"
     elif sql_type_normed.startswith("int"):
-        return "int"
+        return "decimal"
     elif sql_type_normed.startswith("bigint"):
         return "long"
     elif sql_type_normed.startswith("tinyint"):
-        return "int"
+        return "decimal"
     elif sql_type_normed.startswith("smallint"):
-        return "int"
+        return "decimal"
     elif sql_type_normed.startswith("float"):
         return "float"
     elif sql_type_normed.startswith("decimal"):
